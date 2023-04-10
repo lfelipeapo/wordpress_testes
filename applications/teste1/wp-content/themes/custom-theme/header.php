ac7ae7064291104817c99a4df7958464446d7667
Luiz Felipe Apolinário <lfelipeapo@gmail.com>
2023-04-10 01:21:05 -0300

    feat: home sections 1 e 2

--- applications/teste1/wp-content/themes/custom-theme/header.php
+++ applications/teste1/wp-content/themes/custom-theme/header.php
@@ -1,38 +1,33 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
+    <?php wp_body_open(); ?>
     <div id="page" class="site">
         <header>
             <section class="top-bar">
                 <div class="container">
-                    <div class="logo">
-                        <?php 
-                        if( has_custom_logo() ){
+                    <nav class="main-menu">
+                        <div class="menu-items">
+                            <?php
+                        if (has_custom_logo()) {
                             the_custom_logo();
-                        }else{
+                        } else {
                             ?>
-                                <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><span><?php bloginfo( 'name' ); ?></span></a>
+                            <a href="<?php echo esc_url(home_url('/')); ?>"><div class="logo-image"><img src="<?= get_template_directory_uri() . "/assets/images/logo.webp" ?>"></img></div></a>
                             <?php
                         }
                         ?>
-                    </div>                  
+                        <?php wp_nav_menu(array('theme_location' => 'custom_main_menu', 'depth' => 2)); ?>
+                    </div>
+                    </nav>
                 </div>
             </section>
-            <?php 
-            if( is_page( 'home' )): ?>
-            <section class="menu-area">
-                <div class="container">
-                    <nav class="main-menu">
-                        <?php wp_nav_menu( array( 'theme_location' => 'custom_main_menu', 'depth' => 2 )); ?>
-                    </nav>                    
-                </div>
-            </section>
-            <?php endif; ?>
         </header>
--- applications/teste1/wp-content/themes/custom-theme/page-home.php
+++ applications/teste1/wp-content/themes/custom-theme/page-home.php
@@ -1,81 +1,87 @@
 <?php get_header(); ?>
-        <div id="content" class="site-content">
-            <div id="primary" class="content-area">
-                <main id="main" class="site-main">
-                    <?php 
-                    $hero_title = get_theme_mod( 'set_hero_title', __( 'Please, type some title', 'custom' ) );
-                    $hero_subtitle = get_theme_mod( 'set_hero_subtitle', __( 'Please, type some subtitle', 'custom' ) );
-                    $hero_button_link = get_theme_mod( 'set_hero_button_link', '#' );
-                    $hero_button_text = get_theme_mod( 'set_hero_button_text', __( 'Learn More', 'custom' ) );
-                    $hero_height = get_theme_mod( 'set_hero_height', 800 );
-                    $hero_background = wp_get_attachment_url( get_theme_mod( 'set_hero_background' ) );
-                    ?>
-                    <section class="hero" style="background-image: url('<?php echo esc_url( $hero_background ) ?>');">
-                        <div class="overlay" style="min-height: <?php echo esc_attr( $hero_height ) ?>px">
-                            <div class="container">
-                                <div class="hero-items">
-                                    <h1><?php echo esc_html( $hero_title ); ?></h1>
-                                    <p><?php echo nl2br( esc_html( $hero_subtitle ) ); ?></p>
-                                    <a href="<?php echo esc_url( $hero_button_link ) ?>"><?php echo esc_html( $hero_button_text ); ?></a>
-                                </div>
-                            </div>
+<div id="content" class="site-content">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+            <?php
+            $hero_title = 'A gente <strong>transforma</strong> o crédito no Brasil para que você tenha <strong>dinheiro mais barato na hora que precisar</strong>';
+            $hero_subtitle = get_theme_mod('set_hero_subtitle', __('Please, type some subtitle', 'custom'));
+            $hero_button_link = get_theme_mod('set_hero_button_link', '#');
+            $hero_button_text = get_theme_mod('set_hero_button_text', __('Learn More', 'custom'));
+            $hero_height = get_theme_mod('set_hero_height', 675);
+            $hero_background = wp_get_attachment_url(get_theme_mod('set_hero_background'));
+            ?>
+            <section class="hero" style="background-image: url('<?= !isset($hero_background) ? esc_url($hero_background) : get_template_directory_uri() . "/assets/images/section_1_1.webp" ?>');">
+                <div class="overlay" style="min-height: <?php echo esc_attr($hero_height) ?>px">
+                    <div class="container">
+                        <div class="title">
+                            <h1><?= ($hero_title); ?></h1>
                         </div>
-                    </section>
-                    <section class="services">
-                        <h2><?php esc_html_e( 'Services', 'custom' ) ?></h2>
-                        <div class="container">
-                            <div class="services-item">
-                                <?php 
-                                    if( is_active_sidebar( 'services-1' )){
-                                        dynamic_sidebar( 'services-1' );
-                                    }
-                                ?>
-                            </div>
-                            <div class="services-item">
-                                <?php 
-                                    if( is_active_sidebar( 'services-2' )){
-                                        dynamic_sidebar( 'services-2' );
-                                    }
-                                ?>
-                            </div>
-                            <div class="services-item">
-                                <?php 
-                                    if( is_active_sidebar( 'services-3' )){
-                                        dynamic_sidebar( 'services-3' );
-                                    }
-                                ?>
-                            </div>
-                        </div>
-                    </section>
-                    <section class="home-blog">
-                        <h2><?php esc_html_e( 'Latest News', 'custom' ) ?></h2>
-                        <div class="container">
-                            <?php 
+                    </div>
+                </div>
+            </section>
+            <section class="hero-cards" style="max-height: 270px;">
+                <div class="card">
+                    <span class="card-icon">
+                        <img src='<?= get_template_directory_uri() . "/assets/images/section_2_1.webp"; ?>'>
+                    </span>
+                    <h2>Empréstimo Consignado</h2>
+                    <p>para beneficiários do INSS e pagamento a partir de 10 minutos.</p>
+                    <a href="#" class="btn"><strong>Simule agora</strong></a>
+                </div>
+                <div class="card">
+                    <span class="card-icon">
+                        <img src='<?= get_template_directory_uri() . "/assets/images/section_2_1.webp"; ?>'>
+                    </span>
+                    <h2>Empréstimo Consignado</h2>
+                    <p>para beneficiários do INSS e pagamento a partir de 10 minutos.</p>
+                    <a href="#" class="btn"><strong>Simule agora</strong></a>
+                </div>
+                <div class="card">
+                    <span class="card-icon">
+                        <img src='<?= get_template_directory_uri() . "/assets/images/section_2_1.webp"; ?>'>
+                    </span>
+                    <h2>Empréstimo Consignado</h2>
+                    <p>para beneficiários do INSS e pagamento a partir de 10 minutos.</p>
+                    <a href="#" class="btn"><strong>Simule agora</strong></a>
+                </div>
+                <div class="card">
+                    <span class="card-icon">
+                        <img src='<?= get_template_directory_uri() . "/assets/images/section_2_1.webp"; ?>'>
+                    </span>
+                    <h2>Empréstimo Consignado</h2>
+                    <p>para beneficiários do INSS e pagamento a partir de 10 minutos.</p>
+                    <a href="#" class="btn"><strong>Simule agora</strong></a>
+                </div>
+            </section>
+            <section class="home-blog">
+                <div class="container">
+                    <h2><?php esc_html_e('Latest News', 'custom') ?></h2>
+                    <?php
 
-                            $per_page = get_theme_mod( 'set_per_page', 3 );
-                            $category_include = get_theme_mod( 'set_category_include' );
-                            $category_exclude = get_theme_mod( 'set_category_exclude' );
+                    $per_page = get_theme_mod('set_per_page', 3);
+                    $category_include = get_theme_mod('set_category_include');
+                    $category_exclude = get_theme_mod('set_category_exclude');
 
-                            $args = array(
-                                'post_type' => 'post',
-                                'posts_per_page' => esc_html( $per_page ),
-                                'category__in'  => explode( ",", esc_html( $category_include ) ),
-                                'category__not_in' => explode( ",", esc_html( $category_exclude ) )
-                            );
+                    $args = array(
+                        'post_type' => 'post',
+                        'posts_per_page' => esc_html($per_page),
+                        'category__in'  => explode(",", esc_html($category_include)),
+                        'category__not_in' => explode(",", esc_html($category_exclude))
+                    );
 
-                            $postlist = new WP_Query( $args );
+                    $postlist = new WP_Query($args);
 
-                                if( $postlist->have_posts() ):
-                                    while( $postlist->have_posts() ) : $postlist->the_post();
-                                    get_template_part( 'parts/content', 'latest-news' );
-                                    endwhile;
-                                    wp_reset_postdata();
-                                else: ?>
-                                    <p><?php esc_html_e( 'Nothing yet to be displayed!', 'custom' ) ?></p>
-                            <?php endif; ?>                                
-                        </div>
-                    </section>
-                </main>
-            </div>
-        </div>
+                    if ($postlist->have_posts()) :
+                        while ($postlist->have_posts()) : $postlist->the_post();
+                            get_template_part('parts/content', 'latest-news');
+                        endwhile;
+                        wp_reset_postdata();
+                    else : ?>
+                        <p><?php esc_html_e('Nothing yet to be displayed!', 'custom') ?></p>
+                    <?php endif; ?>
+                </div>
+            </section>
+        </main>
+    </div>
+</div>
 <?php get_footer(); ?>
